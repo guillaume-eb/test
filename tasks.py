9c8d3f4cf0057468396d93cfe3ecae596d376e18
Guillaume Carre <guillaume.carre@gmail.com>
2019-07-15 15:12:58 -0700

    Initial commit

---
+++ tasks.py
@@ -0,0 +1,8 @@
+from invoke_release.tasks import *  # noqa: F403
+
+
+# See documentation at https://github.com/eventbrite/invoke-release for help using this and releasing services
+configure_release_parameters(  # noqa: F405
+    module_name='test',
+    display_name='Test Guillaume',
+)
---
+++ test/__init__.py
@@ -0,0 +1,1 @@
+from test.version import __version__, __version_info__  # noqa
